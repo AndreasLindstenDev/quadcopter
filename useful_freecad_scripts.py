f063aea2434e5b288eab38ba8d189aad9b4e2b19
Andreas Lindsten <adde.lindsten@gmail.com>
2025-10-22 20:40:32 +0200

    Created corner joint, ready for printing

--- useful_freecad_scripts.py
+++ useful_freecad_scripts.py
@@ -14,9 +14,9 @@ for i in range(1,14):  # 1 to # inclusive
 
 
 # Select multiple faces of a body in FreeCAD
-doc_name = "Unnamed"
-body_name = "Body001"
+doc_name = "QuadCopter"
+body_name = "Body006"
 
-for i in range(1,45):  # 1 to # inclusive
-    face_name = f"Pocket012.Face{i}"
+for i in range(1,56):  # 1 to # inclusive
+    face_name = f"Pocket028.Face{i}"
     Gui.Selection.addSelection(doc_name, body_name, face_name)
