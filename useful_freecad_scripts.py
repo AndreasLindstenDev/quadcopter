3fdf7269ca9306950982d65abc1b7776dfe03f9a
Andreas Lindsten <adde.lindsten@gmail.com>
2025-11-05 09:57:26 +0100

    Fixed bugs in design of quadcopter center hub 3d print

--- useful_freecad_scripts.py
+++ useful_freecad_scripts.py
@@ -17,6 +17,6 @@ for i in range(1,14):  # 1 to # inclusive
 doc_name = "QuadCopter"
 body_name = "Body007"
 
-for i in range(1,65):  # 1 to # inclusive
-    face_name = f"Pocket034.Face{i}"
+for i in range(1,182):  # 1 to # inclusive
+    face_name = f"Pocket037.Face{i}"
     Gui.Selection.addSelection(doc_name, body_name, face_name)
