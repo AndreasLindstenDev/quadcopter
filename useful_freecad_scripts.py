387161318d2b7e0ee42940af7a42a4ca9dfe5dcc
Andreas Lindsten <adde.lindsten@gmail.com>
2025-11-15 11:28:34 +0100

    fixed more bugs in center_hub, added motor_holder_endpoint for holding the motors and added a drawing of so-so measurments of the tree support beams

--- useful_freecad_scripts.py
+++ useful_freecad_scripts.py
@@ -15,8 +15,8 @@ for i in range(1,14):  # 1 to # inclusive
 
 # Select multiple faces of a body in FreeCAD
 doc_name = "QuadCopter"
-body_name = "Body007"
+body_name = "Body009"
 
-for i in range(1,182):  # 1 to # inclusive
-    face_name = f"Pocket037.Face{i}"
+for i in range(1,50):  # 1 to # inclusive
+    face_name = f"Pocket045.Face{i}"
     Gui.Selection.addSelection(doc_name, body_name, face_name)
